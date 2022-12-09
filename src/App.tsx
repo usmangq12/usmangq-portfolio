1efc0453cac9130f4e7b14e5161e6ec281abb777
usmangq12 <usmangq12@gmail.com>
2022-12-09 19:48:31 +0500

    Added loader.

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,45 @@ import {
   Portfolio,
   Experience,
 } from "./components";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { Logo } from "./components/Logo";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      loading && setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <Grid>
-      <Navbar />
-      <Header />
-      <Container>
-        <Experience />
-        <About />
-        <Skills />
-        <Portfolio />
-      </Container>
-      <Footer />
-    </Grid>
+    <>
+      {loading ? (
+        <Grid
+          height="100vh"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Logo />
+        </Grid>
+      ) : (
+        <Grid>
+          <Navbar />
+          <Header />
+          <Container>
+            <Experience />
+            <About />
+            <Skills />
+            <Portfolio />
+          </Container>
+          <Footer />
+        </Grid>
+      )}
+    </>
   );
 }
 
